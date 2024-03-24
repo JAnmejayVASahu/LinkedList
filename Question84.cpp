935411a3819bce17ded35b112c197f0ea5aec017
Janmejay Sahu <123504424+JAnmejayVASahu@users.noreply.github.com>
2024-03-25 01:53:40 +0530

    Create Question84.cpp
    Given the head of a linked list, delete every alternate elements from the list starting from the second element.

---
+++ Question84.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* next;
+    Node(int data){
+        value = data;
+        next = NULL;
+    }
+};
+class LinkedList{
+    public:
+    Node* head;
+    LinkedList(){
+        head = NULL;
+    } 
+    void insertAtTail(int value){
+        Node* new_node = new Node(value);
+        if(head==NULL){ // linked list is empty
+        head = new_node;
+            return;
+        }
+        Node* temp = head;
+        while(temp -> next != NULL){
+            temp = temp -> next;
+        }
+        temp -> next = new_node;
+    }
+    void display(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout << temp -> value << " -> ";
+            temp = temp -> next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+void deletAlternate(Node* &head){
+    Node* curren_node=head;
+    while(curren_node != NULL && curren_node -> next != NULL){
+        Node* temp = curren_node -> next; // this node to be deleted
+        curren_node -> next = curren_node -> next -> next;
+        free(temp);
+        curren_node = curren_node -> next;
+    }
+}
+int main(){
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(5);
+    ll.display();
+    deletAlternate(ll.head);
+    ll.display();
+    return 0;
+}
