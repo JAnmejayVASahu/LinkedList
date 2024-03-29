657de6159016b2d23ca2146fbf081cf828c67f1e
Janmejay Sahu <123504424+JAnmejayVASahu@users.noreply.github.com>
2024-03-29 16:32:42 +0530

    Create Question146.cpp
    146 insert a node at the starting position

---
+++ Question146.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int value;
+    Node* next;
+    Node(int data){
+        value = data;
+        next = NULL;
+    }
+};
+class CircularLinkedLists{
+    public:
+    Node* head;
+    CircularLinkedLists(){
+        head = NULL;
+    }
+    void display(){
+        Node* temp = head;
+        do{
+            cout << temp -> value <<" -> ";
+            temp = temp -> next;
+        }while(temp != head);
+        cout << endl;
+    }
+    void printCircular(){
+        Node* temp = head;
+        for(int i=0; i<=15; i++){
+            cout << temp -> value <<" -> ";
+            temp = temp -> next;
+        }
+        cout << endl;
+    }
+    void insertAtStart(int value){
+    Node* new_node = new Node(value);
+    if(head == NULL){
+        head = new_node;
+        new_node -> next = head;
+        return;
+        }
+        Node* tail = head;
+        while(tail -> next != head){
+            tail = tail -> next;
+        }
+        // now tail is pointing  to the last  node
+        tail -> next = new_node;
+        new_node -> next = head;
+        head = new_node;
+    }
+};
+int main(){
+    CircularLinkedLists cll;
+    cll.insertAtStart(3);
+    cll.insertAtStart(2);
+    cll.insertAtStart(1);
+    cll.display();
+    cll.printCircular();
+    return 0;
+}
